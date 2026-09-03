e3ca632822d090fb758aaeed8c2a89f502eb93be
Zunayed Ullah <zunayedullah@Mac-2.modem>
2026-09-03 18:08:36 +1000

    Adjustments

--- commands/ping.js
+++ commands/ping.js
@@ -6,6 +6,6 @@ module.exports = {
     .setDescription('Replies with Pong!'),
 
   async execute(interaction) {
-    await interaction.reply('Pong! Luke is a retard!');
+    await interaction.reply(`Pong! Luke is a retard! ${interaction.user.id}, The latency is ${interaction.client.ws.ping}ms!`);
   },
 };
